87989ed670e8d7f813990ea85a4349eaa915d7af
Purushottam Shukla <formalspace@gmail.com>
2020-09-21 07:52:32 +0530

    [BUG-FIX]

--- src/components/DownloadFiles/index.js
+++ src/components/DownloadFiles/index.js
@@ -17,7 +17,7 @@ export default function DownloadFiles({ downloadLinks,expiryCode, fileNames, tot
     const [downloadStatus, updateDownloadStatus] = useState(Array(totalFiles).fill().map(_ => false))
 
     useEffect(() => {
-        if(downloadStatus.filter(i => !!i).length === totalFiles){
+        if(downloadStatus.filter(i => !!i).length === totalFiles && expiryCode === 'onceDownload'){
             fetch(`https://sfo-scheduler.herokuapp.com/kill/:${shareID}`).catch(console.error)
         }
     },[downloadStatus, shareID, totalFiles])
