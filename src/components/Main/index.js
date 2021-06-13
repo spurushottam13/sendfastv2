f768a0b6be95707ec59ea5f326f79b265b230673
spurushottam13 <formalspace@gmail.com>
2021-06-13 17:33:37 +0530

    [Removed]  BuyMeCoffe

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,40 +1,42 @@
-import React, { useState, Fragment } from 'react'
-import './main.css'
-import Download from '../Download'
-import Upload from '../Upload'
-import useDelayUnmount from '../../Hooks/useDelayUnmount'
-import DownloadFiles from '../DownloadFiles'
-import { AppInstall } from '../AppInstall'
-import BuyMeCoffee from '../BuyMeCoffee'
+import React, { useState, Fragment } from "react";
+import "./main.css";
+import Download from "../Download";
+import Upload from "../Upload";
+import useDelayUnmount from "../../Hooks/useDelayUnmount";
+import DownloadFiles from "../DownloadFiles";
+import { AppInstall } from "../AppInstall";
+import BuyMeCoffee from "../BuyMeCoffee";
 export default function Main() {
-    const [isDownload, setIsDownload] = useState(true)
-    const [fileBucket, setFileBucket] = useState()
-    const shouldRenderChild = useDelayUnmount(isDownload, 400);
-    return (
-        <div className="main-section">
-            {
-                shouldRenderChild && (
-                    <Fragment>
-                        <div className="t1">
-                            <h1 className="t2 zero-margin">Now, share files online easily with just 6 digit code</h1>
-                            {/* <div className="t3">No SignUp   No Email   No Phone Number</div> */}
-                        </div>
-                        <Download setIsDownload={setIsDownload} setFileBucket={setFileBucket} isMounted={isDownload} />
-                    </Fragment>
-                )
-            }
-            <div className="content-section">
-            {
-                fileBucket ? (
-                    <DownloadFiles {...fileBucket} />
-                ) : (
-                    <Upload setIsDownload={setIsDownload} />
-                )
-            }
-            </div>
-            <BuyMeCoffee />
-            <AppInstall/>
-            {/* <CircularIndicator/> */}
-        </div>
-    )
+  const [isDownload, setIsDownload] = useState(true);
+  const [fileBucket, setFileBucket] = useState();
+  const shouldRenderChild = useDelayUnmount(isDownload, 400);
+  return (
+    <div className="main-section">
+      {shouldRenderChild && (
+        <Fragment>
+          <div className="t1">
+            <h1 className="t2 zero-margin">
+              Now, share files online easily with just 6 digit code
+            </h1>
+            {/* <div className="t3">No SignUp   No Email   No Phone Number</div> */}
+          </div>
+          <Download
+            setIsDownload={setIsDownload}
+            setFileBucket={setFileBucket}
+            isMounted={isDownload}
+          />
+        </Fragment>
+      )}
+      <div className="content-section">
+        {fileBucket ? (
+          <DownloadFiles {...fileBucket} />
+        ) : (
+          <Upload setIsDownload={setIsDownload} />
+        )}
+      </div>
+      {/* <BuyMeCoffee /> */}
+      <AppInstall />
+      {/* <CircularIndicator/> */}
+    </div>
+  );
 }
