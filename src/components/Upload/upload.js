2cfcd51a3cc28085e16bba8d3cdf90aa0aa62366
spurushottam13 <formalspace@gmail.com>
2022-09-04 04:40:08 +0530

    [Added] Git delete

--- src/components/Upload/upload.js
+++ src/components/Upload/upload.js
@@ -1,106 +1,135 @@
-import firebase from 'firebase/app';
-import 'firebase/storage';
-import 'firebase/database';
-import 'firebase/auth'
-import { generateExpiryTime, getFingerprintId, sleep } from '../../utils';
-import { User } from '../../User';
+import firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/database";
+import "firebase/auth";
+import { generateExpiryTime, getFingerprintId, sleep } from "../../utils";
+import { User } from "../../User";
 
-const FileUpload = (function () {  
-    return {
-        storageRef: firebase.storage().ref(),
-        progress: {
-            percentage: 0,
-            size: 0,
-            currentIndex: 0
-        },
-        bytesTransferredArray: [],
-        dowloadLinks:[],
-        fileNames: [],
-        totalFiles: 0,
-        totalFileSize: 0,
-        upload: function (file, index) {
-            return new Promise((resolve, reject) => {
-                const metadata = {
-                    contentDisposition: `attachment; filename="${file.name}"`,
-                }
-                const uploadTask = this.storageRef.child(this.uid + '/' + file.name).put(file, metadata)
-                uploadTask.on('state_changed', ({ bytesTransferred, totalBytes }) => {
-                    this.bytesTransferredArray[index] = bytesTransferred
-                    const totalBytesTransfered = this.bytesTransferredArray.reduce((a,c) => (a+c))
-                    this.onProgress({
-                        percentage: Math.floor(totalBytesTransfered / this.totalFileSize * 100),
-                        size: `${this.convertBytes(totalBytesTransfered)} / ${this.convertBytes(this.totalFileSize)}`,
-                        file: `${parseInt(index) + 1 }/${this.totalFiles}`,
-                        name: file.name
-                    })
-                },(error) => console.error(error) ,  () => {
-                    uploadTask.snapshot.ref.getDownloadURL().then((url) => {
-                        this.dowloadLinks[index] = url
-                        resolve()
-                    })
-                })
-            })
-        },
-        start: async function ({ bucket, onProgress, uid, expiryCode, onSuccess }) {
-            this.onProgress = onProgress
-            this.uid = uid
-            this.expiryCode = expiryCode
-            bucket.forEach(({ size, name }) => {
-                this.totalFileSize += size
-                this.fileNames.push(name)
-            })
-            this.totalFiles = bucket.length
-            if(!User.auth){
-                await sleep(5000)
-                if(!User.auth) throw new Error("[Auth]: user not logged-in")
-            }
-            for (const index in bucket) {
-                await this.upload(bucket[index], index)
-            }
-            await this.updateDB()
-            this.scheduleKill(uid)
-            if (typeof onSuccess === 'function') {
-                onSuccess(this.dowloadLinks)
-            } else {
-                console.error('Firebase Upload: onSuccess not provided')
-            }
-        },
-        scheduleKill: function(uid){
-            fetch(`https://sfo-scheduler.herokuapp.com/schedule/kill/:${uid}`)
-            .then(r => r.text())
-            .then(console.log)
-            .catch(console.error)
-        },
-        updateDB: function(){
-            return new Promise((resolve, reject) => {
-                firebase.database().ref('UID/'+this.uid).set({
-                    fileNames: this.fileNames,
-                    downloadLinks: this.dowloadLinks,
-                    totalFiles: this.totalFiles,
-                    totalSize: this.totalFileSize,
-                    expiryCode: this.expiryCode,
-                    uploadTime: new Date().getTime(),
-                    expiryTime: generateExpiryTime(this.expiryCode),
-                    fpid: getFingerprintId()
-                })
-                .then(resolve)
-                .catch(_ => console.error("Can not update DB", _))
-            })
-        },        
-        convertBytes: (totalBytes) => {
-            if (totalBytes < 1000000) {
-                return Math.floor(totalBytes / 1000) + 'KB';
-            } else {
-                return Math.floor(totalBytes / 1000000) + 'MB';
-            }
-        }
-    }
-})()
-export default FileUpload
+const FileUpload = (function () {
+  return {
+    storageRef: firebase.storage().ref(),
+    progress: {
+      percentage: 0,
+      size: 0,
+      currentIndex: 0,
+    },
+    bytesTransferredArray: [],
+    dowloadLinks: [],
+    fileNames: [],
+    totalFiles: 0,
+    totalFileSize: 0,
+    upload: function (file, index) {
+      return new Promise((resolve, reject) => {
+        const metadata = {
+          contentDisposition: `attachment; filename="${file.name}"`,
+        };
+        const uploadTask = this.storageRef
+          .child(this.uid + "/" + file.name)
+          .put(file, metadata);
+        uploadTask.on(
+          "state_changed",
+          ({ bytesTransferred, totalBytes }) => {
+            this.bytesTransferredArray[index] = bytesTransferred;
+            const totalBytesTransfered = this.bytesTransferredArray.reduce(
+              (a, c) => a + c
+            );
+            this.onProgress({
+              percentage: Math.floor(
+                (totalBytesTransfered / this.totalFileSize) * 100
+              ),
+              size: `${this.convertBytes(
+                totalBytesTransfered
+              )} / ${this.convertBytes(this.totalFileSize)}`,
+              file: `${parseInt(index) + 1}/${this.totalFiles}`,
+              name: file.name,
+            });
+          },
+          (error) => console.error(error),
+          () => {
+            uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+              this.dowloadLinks[index] = url;
+              resolve();
+            });
+          }
+        );
+      });
+    },
+    start: async function ({ bucket, onProgress, uid, expiryCode, onSuccess }) {
+      this.onProgress = onProgress;
+      this.uid = uid;
+      this.expiryCode = expiryCode;
+      bucket.forEach(({ size, name }) => {
+        this.totalFileSize += size;
+        this.fileNames.push(name);
+      });
+      this.totalFiles = bucket.length;
+      if (!User.auth) {
+        await sleep(5000);
+        if (!User.auth) throw new Error("[Auth]: user not logged-in");
+      }
+      for (const index in bucket) {
+        await this.upload(bucket[index], index);
+      }
+      await this.updateDB();
+      this.scheduleKill(uid, expiryCode);
+      if (typeof onSuccess === "function") {
+        onSuccess(this.dowloadLinks);
+      } else {
+        console.error("Firebase Upload: onSuccess not provided");
+      }
+    },
+    scheduleKill: function (uid, expiryCode) {
+      return new Promise((resolve, reject) => {
+        firebase
+          .database()
+          .ref("kill/" + expiryCode + "/" + uid)
+          .set({
+            fileNames: this.fileNames,
+            downloadLinks: this.dowloadLinks,
+            totalFiles: this.totalFiles,
+            totalSize: this.totalFileSize,
+            expiryCode: this.expiryCode,
+            uploadTime: new Date().getTime(),
+            expiryTime: generateExpiryTime(this.expiryCode),
+            fpid: getFingerprintId(),
+          })
+          .then(resolve)
+          .catch((_) => console.error("Can not update DB", _));
+      });
+    },
+    updateDB: function () {
+      return new Promise((resolve, reject) => {
+        firebase
+          .database()
+          .ref("UID/" + this.uid)
+          .set({
+            fileNames: this.fileNames,
+            downloadLinks: this.dowloadLinks,
+            totalFiles: this.totalFiles,
+            totalSize: this.totalFileSize,
+            expiryCode: this.expiryCode,
+            uploadTime: new Date().getTime(),
+            expiryTime: generateExpiryTime(this.expiryCode),
+            fpid: getFingerprintId(),
+          })
+          .then(resolve)
+          .catch((_) => console.error("Can not update DB", _));
+      });
+    },
+    convertBytes: (totalBytes) => {
+      if (totalBytes < 1000000) {
+        return Math.floor(totalBytes / 1000) + "KB";
+      } else {
+        return Math.floor(totalBytes / 1000000) + "MB";
+      }
+    },
+  };
+})();
+export default FileUpload;
 
 // @type debug: Global variable export
-if(window['SFO']){
-    window['SFO']['fileUpload'] = FileUpload
-}else{
-    window['SFO'] = {fileUpload: FileUpload}
+if (window["SFO"]) {
+  window["SFO"]["fileUpload"] = FileUpload;
+} else {
+  window["SFO"] = { fileUpload: FileUpload };
 }
--- src/components/UploadFileList/index.js
+++ src/components/UploadFileList/index.js
@@ -1,63 +1,83 @@
-import React, { Fragment, useState } from 'react'
-import useModal from '../../Modal'
-import { UploadTerms } from '../Terms'
+import React, { Fragment, useState } from "react";
+import useModal from "../../Modal";
+import { UploadTerms } from "../Terms";
 
-import Icon from '../../Icons'
+import Icon from "../../Icons";
 
-export default function UploadFileList({ fileBucket, isLimitExceed, startUploading, deleteFile }) {
-    const [expiryCode, setExpiryCode] = useState('after24')
-    const [tnc, setTnc] = useState(true)
-    const { showModal, ModalProvider } = useModal()
+export default function UploadFileList({
+  fileBucket,
+  isLimitExceed,
+  startUploading,
+  deleteFile,
+}) {
+  const [expiryCode, setExpiryCode] = useState("after24");
+  const [tnc, setTnc] = useState(true);
+  const { showModal, ModalProvider } = useModal();
 
-    return (
-        <Fragment>
-            <ModalProvider />
-            <div className="file-list">
-                {
-                    fileBucket.map((file, index) => (
-                        <div className="upload-stick-wrapper" key={file.name + index}>
-                            <div className="inline icon-width-limit">
-                                <div>
-                                    {
-                                        file.name.length > 20
-                                            ? (file.name.substr(0, 20) + "..."
-                                            ) : (file.name)
-                                    }
-                                </div>
-                            </div>
-                            <Icon.delete className="delete-icon" onClick={_ => deleteFile(index)} />
-                        </div>
-                    ))
-                }
+  return (
+    <Fragment>
+      <ModalProvider />
+      <div className="file-list">
+        {fileBucket.map((file, index) => (
+          <div className="upload-stick-wrapper" key={file.name + index}>
+            <div className="inline icon-width-limit">
+              <div>
+                {file.name.length > 20
+                  ? file.name.substr(0, 20) + "..."
+                  : file.name}
+              </div>
             </div>
-            <div className="full">
-                <div className="delete-after-wrapper">
-                    <div>Delete after</div>
-                    <select onChange={({ target: { selectedOptions } }) => setExpiryCode(selectedOptions[0].value)}>
-                        <option value="after24" defaultValue={true}>24 hour</option>
-                        {/* <option value="onceDownload">download</option> */}
-                        <option value="1Week">1 week</option>
-                    </select>
-                </div>
-                {
-                    isLimitExceed && (
-                        <div className="limit-warn">
-                            File limit exceeded, please remove {isLimitExceed} of files
-                        </div>
-                    )
-                }
-                <div className="terms-n-condition">
-                    <input type="checkbox" checked={tnc} onChange={({ target: { checked } }) => setTnc(checked)} />
-                    <label
-                        onClick={_ => showModal({ component: UploadTerms, autoDisappear: false })}>
-                        I agree
-                            <span className="pointer text-primary"> Terms & Conditions </span>
-                    </label>
-                </div>
-                <div className={tnc && !isLimitExceed ? "upload-btn" : "upload-btn disable"} onClick={e => (tnc && !isLimitExceed) && startUploading(expiryCode)}>
-                    Upload
-                </div>
-            </div>
-        </Fragment>
-    )
+            <Icon.delete
+              className="delete-icon"
+              onClick={(_) => deleteFile(index)}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="full">
+        <div className="delete-after-wrapper">
+          <div>Delete after</div>
+          <select
+            onChange={({ target: { selectedOptions } }) =>
+              setExpiryCode(selectedOptions[0].value)
+            }
+          >
+            <option value="after24" defaultValue={true}>
+              24 hour
+            </option>
+            <option value="onceDownload">download</option>
+            <option value="1Week">1 week</option>
+          </select>
+        </div>
+        {isLimitExceed && (
+          <div className="limit-warn">
+            File limit exceeded, please remove {isLimitExceed} of files
+          </div>
+        )}
+        <div className="terms-n-condition">
+          <input
+            type="checkbox"
+            checked={tnc}
+            onChange={({ target: { checked } }) => setTnc(checked)}
+          />
+          <label
+            onClick={(_) =>
+              showModal({ component: UploadTerms, autoDisappear: false })
+            }
+          >
+            I agree
+            <span className="pointer text-primary"> Terms & Conditions </span>
+          </label>
+        </div>
+        <div
+          className={
+            tnc && !isLimitExceed ? "upload-btn" : "upload-btn disable"
+          }
+          onClick={(e) => tnc && !isLimitExceed && startUploading(expiryCode)}
+        >
+          Upload
+        </div>
+      </div>
+    </Fragment>
+  );
 }
